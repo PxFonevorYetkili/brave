50ef426d90e872058ce01d69915751619ffe8c2b
Shirzadov <62316756+Shirzadov@users.noreply.github.com>
2021-11-19 18:04:09 +0300

    Add files via upload
    https://discord.gg/7xzQdYNGNB Lynx Code

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  token: "",
+  prefix: '!',
+  developers: [ "451474544255959071" ],
+  mongourl: "mongodb+srv://Test1:Test1@cluster0.ubo0e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  links: {
+    support: "https://discord.gg/7xzQdYNGNB"
+  },
+  filteredCmds: [ "davet", "yardım", "eval", "karaliste", "bakım", "coin", "kod", "premium", "ping" ],
+  embed: {
+    "image": "https://cdn.discordapp.com/attachments/840662526617714708/845809978332676096/light-banner.png"
+  }
+};
