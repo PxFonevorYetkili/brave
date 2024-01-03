312a00a1acd10447fe1e40b46a1bf1daf157849d
PxFonevorYetkili <155456380+PxFonevorYetkili@users.noreply.github.com>
2024-01-03 19:01:01 +0400

    bug-bildir.js

--- commands/bug-bildir.js
+++ commands/bug-bildir.js
@@ -5,7 +5,7 @@ module.exports = {
   let lang = client.locale(client[`serverData_${message.guild.id}`].language)['bugbildir'];
   const bug = args.slice(0).join(" ");
   if(!bug) return message.channel.error(lang['belirt']);
-  const hook = new Discord.WebhookClient("842186434765258793", "jPVSzwWPHZQOtEBW6xn8BY91BKV2TSkH4Xx2XDVT-usc9JPQ9-YdOPoC0xjuFHf8BjTP");
+  const hook = new Discord.WebhookClient("1192111032383057960", "JIk5nU29mcZNpEZ5eHEQCkS-mc7nHZPBcgE2l-a6WO3QQEeMYyqJLzFPfRkW74oHO9ii");
   hook.send(new Discord.MessageEmbed().setTitle("Bug tespit edildi...").setColor("#F4F4F4").setDescription(`
   > **Bug bulan**: ${message.author.tag} \`(${message.author.id})\`
   > **Sunucu**: ${message.guild.name} \`(${message.guild.id})\`
