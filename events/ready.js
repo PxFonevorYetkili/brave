37f52ffae6c688ed702407ce7db49eb1f19d50fc
PxFonevorYetkili <155456380+PxFonevorYetkili@users.noreply.github.com>
2024-01-03 18:50:05 +0400

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = (Discord, client, config) => {
   client.on('ready', () => {
       console.log('(!) ' + client.user.tag + ' olarak oturum açıldı!');
       client.user.setStatus('idle');
-      client.user.setActivity('Levian Code | '+client.guilds.cache.size+' servers!', {
+      client.user.setActivity(Mutlu yıllar! | '+client.guilds.cache.size+' servers!', {
           type: 'LISTENING'
       }).then(() => {
           console.log('(!) Presence başarıyla ayarlandı!');
