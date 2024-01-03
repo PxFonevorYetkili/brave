0bc9f6ab6b0a66b513b1f5c25d6658ed3307f73f
PxFonevorYetkili <155456380+PxFonevorYetkili@users.noreply.github.com>
2024-01-03 18:56:08 +0400

    davet.js

--- commands/davet.js
+++ commands/davet.js
@@ -8,7 +8,7 @@ module.exports = {
             .setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true }))
             .setColor("BLURPLE").setImage(config.embed.image)
             .setDescription(`${lang['bot']['davetMesaj'].replace("{author}", message.author.username)}`)
-            .addField(`${lang['bot']['links']}`, `**[${lang['bot']['invite']}](https://lightpartner.xyz/invite) | [${lang['bot']['support']}](${config.links.support})**`)
+            .addField(`${lang['bot']['links']}`, `**[${lang['bot']['invite']}](https://discord.com/api/oauth2/authorize?client_id=1191768101298110536&permissions=8&scope=bot) | [${lang['bot']['support']}](${config.links.support})**`)
         )
   },
     config: {
